91aeaf39e6237a05ff2b834aea4d7ad70a2b2e42
gabriele-andolina <gabe.andolina@gmail.com>
2023-02-14 22:07:56 +0100

    Modified start() to arrow f. to solve 'this' issue

--- script.js
+++ script.js
@@ -7,9 +7,25 @@ class Timer {
         this.startButton.addEventListener('click', this.start);
     }
 
-    start() {
-        console.log('Start my timer!')
+    // A note: the following method equals to:
+    // start: function() {
+    //     console.log(this);
+    // }
+    // However, there is a problem with this syntax, due to the keyword 'this'.
+    // In fact, the following code logs the startButton.
+    // start() {
+    //     console.log(this)
+    // }
+    // To avoid this behaviour and make sure the 'start' method refers to the Timer class, 
+    // we need to use an arrow function like so:
+    start = () => {
+        console.log(this, 'I\'M THIS!')
     }
+    // By using an arrow function, the 'start' method is defined behind the scenes as a property of 'this',
+    // 'this' being the actual instance of the Timer class.
+    // This solution allows me to solve the 'this' issue in a (more) modern way.
+    // FYI: There are also other more advanced solutions, like calling 'bind', 'call' or 'apply' on the method.
+    // For more info, see video no. 203 of 'The Modern JavaScript Bootcamp' by Colt Steele.
 }
 
 const durationInput = document.querySelector('#duration');
