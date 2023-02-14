181676e898b1c791d798a30474cc58a0acc9570b
gabriele-andolina <gabe.andolina@gmail.com>
2023-02-14 14:49:10 +0100

    Created new Timer instance variable

--- script.js
+++ script.js
@@ -15,3 +15,5 @@ class Timer {
 const durationInput = document.querySelector('#duration');
 const startButton = document.querySelector('#start');
 const pauseButton = document.querySelector('#pause');
+
+const timer = new Timer(durationInput, startButton, pauseButton);
