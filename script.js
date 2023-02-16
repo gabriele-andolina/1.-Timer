0d631f8cd0b0f001d4dc14e780ed9781eef7f900
gabriele-andolina <gabe.andolina@gmail.com>
2023-02-16 18:43:33 +0100

    Added conditional logic to stop timer at 0 sec

--- script.js
+++ script.js
@@ -15,13 +15,11 @@ class Timer {
     };
     
     tick = () => {
-        // Compared to the previous commit, here I'm condensing all the logic into one line.
-        // What's happening here, behind the scenes, is that:
-        // 1) on the right-hand side of the equal sign, I'm calling the get method to retrieve 
-        // the value of the input (the remaining time we're at at a specific point in time);
-        // 2) on the left-hand side of the equal sign, I'm calling the set method to set the updated 
-        // remaining time.
-        this.timeRemaining = this.timeRemaining - 1;
+        if (this.timeRemaining <= 0) {
+            this.pause();
+        } else {
+          this.timeRemaining = this.timeRemaining - 1;
+        }
     };
 
     pause = () => {
