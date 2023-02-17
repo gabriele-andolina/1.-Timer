b8b475a7086e1769e40015b56c7919e915fa964a
gabriele-andolina <gabe.andolina@gmail.com>
2023-02-17 12:55:47 +0100

    Added callback functions to Timer class methods

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ class Timer {
         this.pauseButton = pauseButton;
         if (callbacks) {
             this.onStart = callbacks.onStart;
+            this.onTick = callbacks.onTick;
+            this.onComplete = callbacks.onComplete;
         }
     
         this.startButton.addEventListener('click', this.start);
@@ -22,8 +24,14 @@ class Timer {
     tick = () => {
         if (this.timeRemaining <= 0) {
             this.pause();
+            if (this.onComplete) {
+                this.onComplete();
+            }
         } else {
           this.timeRemaining = this.timeRemaining - 1;
+          if (this.onTick) {
+            this.onTick();
+          }
         }
     };
 
@@ -46,12 +54,12 @@ const pauseButton = document.querySelector('#pause');
 
 const timer = new Timer(durationInput, startButton, pauseButton, {
     onStart() {
-        console.log('Timer has started!')
+        console.log('Timer has started.')
     },
     onTick() {
-
+        console.log('Timer just ticked down.')
     },
     onComplete() {
-
+        console.log('Timer is completed.')
     }
 });
